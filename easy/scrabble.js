2dc715d30d269a6f7181c688419bef0dd3d3c8af
HDKHALILI <14176079+HDKHALILI@users.noreply.github.com>
2021-01-25 13:44:52 +1100

    refactor scrable.js

--- easy/scrabble.js
+++ easy/scrabble.js
@@ -3,24 +3,21 @@ class Scrable {
     return new Scrable(word).score();
   }
 
-  static getScore(char) {
-    if ("AEIOULNRST".includes(char)) {
-      return 1;
-    } else if ("DG".includes(char)) {
-      return 2;
-    } else if ("BCMP".includes(char)) {
-      return 3;
-    } else if ("FHVWY".includes(char)) {
-      return 4;
-    } else if (char === "K") {
-      return 5;
-    } else if ("JX".includes(char)) {
-      return 8;
-    } else if ("QZ".includes(char)) {
-      return 10;
-    }
+  static POINTS = {
+    AEIOULNRST: 1,
+    DG: 2,
+    BCMP: 3,
+    FHVWY: 4,
+    K: 5,
+    JX: 8,
+    QZ: 10,
+  };
 
-    return 0;
+  static getPoint(char) {
+    let key = Object.keys(Scrable.POINTS).find(letters =>
+      letters.includes(char)
+    );
+    return Scrable.POINTS[key] || 0;
   }
 
   constructor(word) {
@@ -29,7 +26,7 @@ class Scrable {
 
   score() {
     return this.chars.reduce((totalScore, char) => {
-      return totalScore + Scrable.getScore(char);
+      return totalScore + Scrable.getPoint(char);
     }, 0);
   }
 }
